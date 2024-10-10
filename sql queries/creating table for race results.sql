5b5cc0eeaa876beecd70ec05647126d190faf4f0
Tammy <tammylicarrick@gmail.com>
2024-10-10 01:16:44 -0400

    renamed results table and added the racer id column

--- sql queries/creating table for race results.sql
+++ sql queries/creating table for race results.sql
@@ -1,11 +1,12 @@
 -- this file creates a table for race results
-IF OBJECT_ID('toronto10k_results', 'U')IS NOT NULL
+IF OBJECT_ID('race_results', 'U')IS NOT NULL
 BEGIN
-	DROP TABLE toronto10k_results
+	DROP TABLE race_results
 END
 
 SELECT
 	CAST(LEFT(overall_place, LEN(overall_place) - 5) AS INT) AS overall_place,
+	racerID,
 	gun_time,
 	chip_time
 INTO race_results
@@ -15,5 +16,5 @@ FROM toronto10k;
 ALTER TABLE race_results
 ALTER COLUMN overall_place INT NOT NULL;
 
-ALTER TABLE races_results
+ALTER TABLE race_results
 ADD CONSTRAINT PK_overall_place PRIMARY KEY (overall_place);
