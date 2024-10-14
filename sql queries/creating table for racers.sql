b5be2575d1321ca3eb0688eed53fb515b2074f86
Tammy <tammylicarrick@gmail.com>
2024-10-14 05:37:31 -0400

    updated racers table

--- sql queries/creating table for racers.sql
+++ sql queries/creating table for racers.sql
@@ -8,16 +8,36 @@ SELECT
 	(RIGHT(division, LEN(division) - 1)) AS age 
 FROM toronto10k
 )
--- format gender and age
+-- creating racer table from query
 SELECT 
 	racerID,
 	name,
 	gender,
 	CASE
 		WHEN age = '-OPEN' THEN 'Open'
-		WHEN age LIKE '%U%' THEN 'U20'
-		WHEN age LIKE '____' THEN LEFT(age, 2) + '-' + RIGHT(age, 2)
-		WHEN age LIKE '___' THEN RIGHT(age,3)
+		-- combine U20 with 20-29 group
+		WHEN age LIKE '%U%' OR age = '2029' THEN '19-29'
+		-- combine 90+ with 60-69 age group
+		WHEN age = '90+' OR age = '6069' THEN '60+'
+		WHEN age LIKE '[345]___' THEN LEFT(age, 2) + '-' + RIGHT(age, 2)
 	END AS ageBracket
 INTO racers
 FROM gender_age;
+
+-- make racerID the primary key
+ALTER TABLE racers
+ADD CONSTRAINT PK_racerID PRIMARY KEY(racerID)
+
+-- adding a foregin key constraint 
+ALTER TABLE race_results
+ADD CONSTRAINT FK_racerID
+FOREIGN KEY (racerID)
+REFERENCES racers (racerID)
+ON DELETE CASCADE;
+
+-- delete racers in the open category
+DELETE FROM racers
+WHERE ageBracket = 'Open'
+
+
+
